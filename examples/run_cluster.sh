904dec7e69d47e4ca4b4fc9fbbb447aa2a41b509
Dmitry Orlov <me@mosquito.su>
2017-01-09 14:15:14 +0300

    [fix] return the sh script

---
+++ examples/run_cluster.sh
@@ -0,0 +1,12 @@
+# Add raftos package to PYTHONPATH so we don't need to install it to run example
+export PYTHONPATH="${PYTHONPATH}:$(pwd)/../"
+
+# Remove previous data
+rm -f *.log
+rm -f *.storage
+rm -f *.state_machine
+
+# Start
+../env/bin/python node.py --node "8000" --cluster "8000 8001 8002" &
+../env/bin/python node.py --node "8001" --cluster "8000 8001 8002" &
+../env/bin/python node.py --node "8002" --cluster "8000 8001 8002" &
